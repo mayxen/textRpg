e8da0bb534f6e60ac50b85f2ffaae68f5ff0ccbe
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2018-11-24 12:25:05 +0000

    1
    empece con el desarrollo de un personaje

---
+++ Assets/Proyect/Script/Character.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character: MonoBehaviour
+{
+    public int Energy{ get; set; }
+    public int Attack{ get; set; }
+    public int Defence{ get; set; }
+    public int Gold { get; set; }
+    public Vector2 RoomIndex{ get; set; }
+    public List<string> Inventory { get; set; }
+
+}
