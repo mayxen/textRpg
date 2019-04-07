c4142bb2d6426a10fbfa9155af2c414cb4d3b4e2
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2019-04-07 13:01:33 +0100

    barbarian move and some fix

---
+++ Assets/Proyect/Script/MovePlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public int speed;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direcction = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.GetComponent<Rigidbody>().velocity = direcction;
+        if(direcction != Vector3.zero)
+            transform.rotation = Quaternion.LookRotation(direcction);
+    }
+}
